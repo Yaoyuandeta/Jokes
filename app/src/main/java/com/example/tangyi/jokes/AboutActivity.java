6069e7a212220c2efb7a8519570e0cf14154b6ec
TangYi <358181251@qq.com>
2016-06-04 03:48:44 +0800

    6.4 3:48 min

---
+++ app/src/main/java/com/example/tangyi/jokes/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.example.tangyi.jokes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+/**
+ * Created by 在阳光下唱歌 on 2016/6/4.
+ */
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.about_activity);
+    }
+}
--- app/src/main/java/com/example/tangyi/jokes/Tools/MyFragment.java
+++ app/src/main/java/com/example/tangyi/jokes/Tools/MyFragment.java
@@ -1,21 +1,33 @@
 package com.example.tangyi.jokes.Tools;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.example.tangyi.jokes.AboutActivity;
 import com.example.tangyi.jokes.R;
 
 /**
  * Created by 在阳光下唱歌 on 2016/5/11.
  */
 public class MyFragment extends Fragment {
+    private LinearLayout aboutLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.setting_layout,null);
+        aboutLayout=(LinearLayout)view.findViewById(R.id.about_layout);
+        aboutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),AboutActivity.class));
+            }
+        });
         return view;
     }
 }
